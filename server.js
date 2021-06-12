3fa272d28d85d64560f4faf97791e281e0aabe1d
minhdz117 <minhdz117@gmail.com>
2021-06-12 15:41:23 +0900

    ok

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app=express()
 app.set('views','/views')
 app.set('view engine','ejs')
 
-var text={"hello.txt":`DFE8F6EA2BC50A4CAC6AE07BE2CF33FCF1F816A2BCEC6461290049AEF1C1DC6B
+var text={"hello.txt":`0B0C156080E9F88D4E048B2AA93C8ED3B59BA060FF5004B064314C12341EB579
 comodoca.com
-d023f29b4d3d1be`};
+8437dcc21dd3cef`};
 app.get('/.well-known/pki-validation/840FFF573D8DCC62041AF9E0C85FBA6C.txt',function(req,res){
     res.set({"Content-Disposition":"attachment; filename=\"840FFF573D8DCC62041AF9E0C85FBA6C.txt\""});
     res.send(text['hello.txt']);
